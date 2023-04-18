0a71713bdc590fb1f0851edfb4198d5c1f6725a2
usr2775 <usr2775@salesianos-lacuesta.net>
2023-04-18 16:17:09 +0100

    feat: cambiar nombre del contructor

--- src/PokemonL.java
+++ src/PokemonL.java
@@ -1,7 +1,7 @@
 public class PokemonL {
     private String localizacion;
 
-    public PokemonLegendario(String nombre, String tipo, String descripcion) {
+    public PokemonL(String nombre, String tipo, String descripcion) {
         super(nombre, tipo);
         this.localizacion = localizacion;
     }

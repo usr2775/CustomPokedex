39560fdfc863ca1b6cb6a5b421cec816cd32b199
usr2775 <usr2775@salesianos-lacuesta.net>
2023-04-18 13:04:23 +0100

    fix: arreglado de indentado

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,18 +1,17 @@
 public class Pokemon {
-    
-        private String nombre;
-        private String tipo;
-        private double altura;
-        private double peso;
-        private String descripcion;
 
-        public Pokemon(String nombre, String tipo) {
-            this.nombre = nombre;
-            this.tipo = tipo;
-            this.altura = altura;
-            this.peso = peso;
-            this.descripcion = descripcion;
-        }       
-    
+    private String nombre;
+    private String tipo;
+    private double altura;
+    private double peso;
+    private String descripcion;
+
+    public Pokemon(String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.altura = altura;
+        this.peso = peso;
+        this.descripcion = descripcion;
+    }
+
 }
-

932ad945e9929d6ed2021a256bd7d2aff33ba985
usr2775 <usr2775@salesianos-lacuesta.net>
2023-04-18 16:14:02 +0100

    fix: arreglado del indentado

--- src/PokemonL.java
+++ src/PokemonL.java
@@ -5,12 +5,13 @@ public class PokemonL {
         super(nombre, tipo);
         this.localizacion = localizacion;
     }
+
     public String getLocalizacion() {
         return localizacion;
     }
+
     @Override
     public String toString() {
         return super.toString() + "\nLocalización: " + (localizacion == null ? "???" : localizacion);
     }
 }
-
